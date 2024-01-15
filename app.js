02a6dc5b4bf6529a6029f9b0459001cce3ccbcc3
VladPashkovsky <v.v.pashkovsky@gmail.com>
2024-01-15 15:24:58 +0100

    v.2.0

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const cols = document.querySelectorAll('.col')
 
+// При нажатии на пробел меняем цвет, а также убираем поведение по-умолчанию, чтобы при нажатии обводка вокруг
+// кнопки не оставалась
 document.addEventListener('keydown', (event) => {
   event.preventDefault()
   if (event.code.toLowerCase() === 'space') {
@@ -7,6 +9,9 @@ document.addEventListener('keydown', (event) => {
   }
 })
 
+//Меняем замок на закрытый при клике. Предварительно на button вешаем data-type='lock'.
+// Его также необходимо продублировать на саму иконку, чтобы клик работал и при нажатии на картинку с замком.
+// В конце просто type, потому что data он опускает
 document.addEventListener('click', (event) => {
   const type = event.target.dataset.type
 
@@ -23,7 +28,7 @@ document.addEventListener('click', (event) => {
   }
 })
 
-function gerenerateRandomColor() {
+function generateRandomColor() {
   // RGB
   // #FF0000
   // #00FF00
@@ -37,10 +42,21 @@ function gerenerateRandomColor() {
   return '#' + color
 }
 
+// Функция для копирования выбранного вета при нажатии. Используем ее там, где обрабатываем click в блоке else
+// На h2 вешаем data-type='copy'
 function copyToClickboard(text) {
   return navigator.clipboard.writeText(text)
 }
 
+// Вместо функции generateRandomColor() можно было использоать библиотеку chroma, метод chroma.random(), подключаем
+// с помощью cdn в html
+
+// Если случайный цвет и цвет текста, и кнопки-замка будут сливаться, то (используя библиотеку chroma):
+
+//Основная функция по изминению цвета, если 'fa-lock', цвет больше не меняется.
+// Метод contains для проверки на вложенность
+// var result = parent.contains(child);
+// Возвращает true, если parent содержит child или parent == child.
 function setRandomColors(isInitial) {
   const colors = isInitial ? getColorsFromHash() : []
 
@@ -57,8 +73,8 @@ function setRandomColors(isInitial) {
     const color = isInitial
       ? colors[index]
         ? colors[index]
-        : chroma.random()
-      : chroma.random()
+        : generateRandomColor()
+      : generateRandomColor()
 
     if (!isInitial) {
       colors.push(color)
@@ -74,11 +90,14 @@ function setRandomColors(isInitial) {
   updateColorsHash(colors)
 }
 
+// Если случайный цвет и цвет текста, и кнопки-замка будут сливаться, то (используя библиотеку chroma):
 function setTextColor(text, color) {
   const luminance = chroma(color).luminance()
   text.style.color = luminance > 0.5 ? 'black' : 'white'
 }
 
+//Функция, которая будет выбранные цвета добавлять в хэш, чтобы в дальнейшем этим можно будет с кем-нибудь делиться
+// Используем ее setRandomColors()
 function updateColorsHash(colors = []) {
   document.location.hash = colors
     .map((col) => {
